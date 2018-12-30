855f3337f39b3af8a6bca75e28ccaf69ab665c79
Ryan Johanson <rjohanson@pilotfishtechnology.com>
2018-12-30 21:33:53 +0200

    Initial Commit

--- src/main/java/com/mtg/ws/controller/LoginController.java
+++ src/main/java/com/mtg/ws/controller/LoginController.java
@@ -1,5 +1,6 @@
-package com.mtg;
+package com.mtg.ws.controller;
 
+import com.mtg.LoginBean;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
