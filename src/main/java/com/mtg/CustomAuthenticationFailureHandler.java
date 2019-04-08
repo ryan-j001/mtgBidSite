3aadf00ab8c959307f97455173ce93641751ae75
Ryan Johanson <rjohanson@pilotfishtechnology.com>
2019-04-08 06:47:51 +0200

    Initial Commit

---
+++ src/main/java/com/mtg/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,45 @@
+package com.mtg;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class CustomAuthenticationFailureHandler
+        implements AuthenticationFailureHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private final static Logger LOGGER = Logger.getLogger(CustomAuthenticationFailureHandler.class.getName());
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationFailure(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException exception)
+            throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        Map<String, Object> data = new HashMap<>();
+        data.put(
+                "timestamp",
+                Calendar.getInstance().getTime());
+        data.put(
+                "exception",
+                exception.getMessage());
+
+        redirectStrategy.sendRedirect(request, response, "/loginError");
+
+    }
+}
